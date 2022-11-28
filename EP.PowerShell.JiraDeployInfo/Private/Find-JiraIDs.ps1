a2aa331a37255949e62a9e2389208d7993c18707
Andy Bolton <andy.bolton@smartwyre.com>
2022-11-28 13:25:42 -0700

    Update regex pattern to match our usecase

--- EP.PowerShell.JiraDeployInfo/Private/Find-JiraIDs.ps1
+++ EP.PowerShell.JiraDeployInfo/Private/Find-JiraIDs.ps1
@@ -2,7 +2,7 @@ function Find-JiraIDs {
     param (
         $message
     )
-    $pattern = '\w{2,10}-\d{1,4}'
+    $pattern = "(SMART|OPS)-\d*"
     $values = [regex]::Matches($message, $pattern) | Select-Object value 
     Write-Debug ("[JIRA IDs] " + $values)
     $values
